b4ff870bd9e289ec4c1212e0c547dd5bf5c7ab6d
Heidi Konswah <heidikonswah@gmail.com>
2017-07-23 10:30:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     navigator.geolocation.getCurrentPosition(function(position) {
       var lat = position.coords.latitude;
       var lon = position.coords.longitude;
-      var url = 'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&units=metric&appid=7b6fd3efc84f91cabf71b2c81ba20e63';
+      var url = 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&units=metric&appid=7b6fd3efc84f91cabf71b2c81ba20e63';
       $.getJSON(url, function(data){
         var desc = data.weather[0].description;
         var icon = data.weather[0].icon;
